56276c1aa5038b94e7d251acdbbe6a22c5b53c3c
Samih Ouague <samih.ouaguenouni@gmail.com>
2021-01-26 13:41:39 +0100

    OK

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Entity\UserEntity;
 use App\Form\RegistrationFormType;
 use App\Security\CustomFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,7 +23,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('home');
         }
         
-        $user = new User();
+        $user = new UserEntity();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
